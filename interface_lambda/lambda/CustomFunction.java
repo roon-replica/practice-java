d976bd163eb445e9a9c4e3a08fca284b4362a203
leemr <think-roon@naver.com>
2022-05-15 23:31:23 +0900

    functional interface에 추상 메서드가 여러개? -> Object에 정의된 것들은 카운트 안 함

---
+++ interface_lambda/lambda/CustomFunction.java
@@ -0,0 +1,13 @@
+package interface_lambda.lambda;
+
+@FunctionalInterface
+public interface CustomFunction {
+    void print();
+
+    //  equals, toString이 추상메서드처럼 보여도 java.lang.Object에 구현되어 있는 메서드들은 추상 메서드로 세아리지 않음.
+    boolean equals(Object o);
+
+    String toString();
+
+
+}
